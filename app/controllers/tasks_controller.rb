196d2ed9512efbbe9ee80a36a29cb8282d761e4d
hato.popo <n4n.yk5.b@gmail.com>
2020-11-25 10:02:21 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy, :edit, :update, :show]
+  
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -12,7 +14,8 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    # @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "Taskが正常に投稿されました"
@@ -24,12 +27,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-    
     if @task.update(task_params)
       flash[:success] = "Taskは正常に更新されました"
       redirect_to @task
@@ -40,7 +40,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = "Taskは正常に削除されました"
@@ -52,5 +51,12 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   
   get 'signup', to: 'users#new'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :tasks
-  resources :users, only: [:index, :new, :create]
+  resources :users, only: [:new, :create]
 end
